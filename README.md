4188a8e36b8102bda5196c8c0af4757acab8a3a3
farisubuntu <farisubuntu@gmail.com>
2024-03-16 11:31:47 +0300

    sdf

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-express-typescript
 template for node, express and typescript
+
+from [https://ultimatecourses.com/blog/setup-typescript-nodejs-express](https://ultimatecourses.com/blog/setup-typescript-nodejs-express)
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express,{Express,Request,Response} from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+
+dotenv.config()
+
+const PORT=process.env.PORT || 3000;
+
+const app:Express=express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req:Request,res:Response)=>{
+  res.send('<h1>Hello from typescript and express server</h1>');
+});
+
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
